2095c9b665fd9248214591f6dfdf811d03f6762f
Jean Pieer <jeanpieer.pb@gmail.com>
2025-05-28 22:41:00 -0500

    Completando la programación

--- ejercicio37.php
+++ ejercicio37.php
@@ -3,21 +3,32 @@ if(isset($_POST['numero'])){
     $num=$_POST['numero'];
     $suma=0;
     $serie=0;
+    $termino = 1;
+    $sumarDos = true;
 }else{
     $num="";
     $suma=0;
     $serie=0;
+    $termino = 1;
+    $sumarDos = true;
 }
 echo "<form method=post style='background-color: lightblue'>
-<h2>Mostrar los primeros 'N' terminos de la serie y sumarlos / Multiplo de 5</h2>
-Ingrese el número de terminos <input type=number name=numero value=$num><br>
+<h2>Mostrar los primeros 'N' terminos de la serie y sumarlos</h2>
+Ingrese el número de terminos <input type=number name=numero min=1 value=$num><br>
 <input type=submit value=Mostrar>
 </form>";
-echo "Serie: ";
-for ($i=1;$i<=$num;$i++) {
-    $serie= 1 * $i;
+echo "SERIE: ";
+for ($i=1; $i <= $num ; $i++){
+    $serie=$termino;
     $suma+=$serie;
+    if ($sumarDos) {
+        $termino += 2;
+    } else {
+        $termino += 1;
+    }
+    $sumarDos = !$sumarDos;
+
     echo " $serie, ";
 }
-echo "<br>Suma: $suma";
+echo "<br>SUMA: $suma ";
 ?>
